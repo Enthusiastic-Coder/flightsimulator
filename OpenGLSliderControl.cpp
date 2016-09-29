5706a13f00c2b0f595d8a8375268653b48c2600a
Mo <jebaramo@gmail.com>
2016-09-29 19:30:27 +0100

    wip - intial layout of class for (VH) slider control

--- OpenGLSliderControl.cpp
+++ OpenGLSliderControl.cpp
@@ -1,1 +1,59 @@
 #include "OpenGLSliderControl.h"
+#include <OpenGLRenderer.h>
+
+void OpenGLSliderControl::setPosition(MathSupport<int>::point pos)
+{
+    _position = pos;
+    updateLayout();
+}
+
+void OpenGLSliderControl::setLength(int length)
+{
+    _length = length;
+    updateLayout();
+}
+
+void OpenGLSliderControl::setWidth(int width)
+{
+    _width = width;
+    updateLayout();
+}
+
+void OpenGLSliderControl::setOrientation(OpenGLSliderControl::Orientation o)
+{
+    _orientation = o;
+    updateLayout();
+}
+
+void OpenGLSliderControl::setHAlignment(OpenGLSliderControl::Alignment alignment)
+{
+    _hAlignment = alignment;
+    updateLayout();
+}
+
+void OpenGLSliderControl::setVAlignment(OpenGLSliderControl::Alignment alignment)
+{
+    _vAlignment = alignment;
+    updateLayout();
+}
+
+void OpenGLSliderControl::setColor(Vector4F color)
+{
+    _color = color;
+}
+
+void OpenGLSliderControl::update(float dt)
+{
+    // update current value to match value based on dt
+
+}
+
+void OpenGLSliderControl::render(Renderer *r)
+{
+
+}
+
+void OpenGLSliderControl::updateLayout()
+{
+    // use all state to decide how to position control
+}
--- OpenGLSliderControl.h
+++ OpenGLSliderControl.h
@@ -4,6 +4,8 @@
 #include <MathSupport.h>
 #include <vector4.h>
 
+class Renderer;
+
 class OpenGLSliderControl
 {
 public:
@@ -21,18 +23,33 @@ public:
     };
 
     void setPosition( MathSupport<int>::point pos);
+    void setLength(int length);
+    void setWidth(int width);
+    void setOrientation(Orientation o);
+    void setHAlignment(Alignment alignment);
+    void setVAlignment(Alignment alignment);
+    void setColor(Vector4F color);
 
+    void update(float dt);
+    void render(Renderer* r);
+
+protected:
+    void updateLayout();
 
 private:
     MathSupport<int>::point _position;
     MathSupport<int>::size _size;
     Vector4F _color = Vector4F(1,1,1,1);
+    Orientation _orientation = {};
     Alignment _hAlignment = {};
     Alignment _vAlignment = {};
-    int _currentValue = 0;
-    int _value = 0;
-    int _min = 0;
-    int _max = 10;
+    int _length = 1;
+    int _width = 1;
+    float _currentValue = 0;
+    float _value = 0;
+    float _min = 0;
+    float _max = 10;
+    float _rateOfValue = 1.0;
 };
 
 #endif
