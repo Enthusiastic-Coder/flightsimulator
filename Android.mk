a30fd5f1a5c3b117a31b53f5130cc78b746e4949
Samsung15 <jebaramo@gmail.com>
2016-10-20 18:41:32 +0100

    wip - update to Android.mk

--- Android.mk
+++ Android.mk
@@ -4,19 +4,97 @@ LOCAL_PATH := $(TOP_LOCAL_PATH)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/glm $(LOCAL_PATH)/sdk/include 
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/rapidjson/include $(LOCAL_PATH)/sdk/include 
 LOCAL_CFLAGS +=  -DRAPIDJSON_HAS_STDSTRING=1
 
 LOCAL_SRC_FILES := sdl2/src/main/android/SDL_android_main.c \
-		camera.cpp \
-		matrices.cpp \
-		mesh.cpp \
-		meshLoader.cpp \
-		opengl.cpp \
-		shader.cpp \
+			sdlmain.cpp \
+           SDLGameLoop.cpp \
+           SDLMainWindow.cpp \
+           TextureLoader.cpp \
+            SDLSurfaceHelper.cpp \
+		AeroControlSurface.cpp \
+		AeroControlSurfaceBoundary.cpp \
+		AeroForceGenerator.cpp \
+		AeroSectionElementForceGenerator.cpp \
+		AeroSectionForceGenerator.cpp \
+		AeroSectionSubElementForceGenerator.cpp \
+		AeroWheelSpringForceGenerator.cpp \
+		AircraftCarrierRigidBody.cpp \
+		AircraftEngineForceGenerator.cpp \
+		AircraftEngineNozzleBleeder.cpp \
+		AircraftEngineNozzleFeeder.cpp \
+		AirProperties.cpp \
+		AoaClData.cpp \
+		BA320RigidBody.cpp \
+		Camera.cpp \
+		ConstrainedSpringForceGenerator.cpp \
+		CPPSourceCodeMeshModel.cpp \
+		DataRecorder.cpp \
+		FlightRecorder.cpp \
+		ForceGenerator.cpp \
+		GPSTileBoundary.cpp \
+		GravityForceGenerator.cpp \
+		GSForceGenerator.cpp \
+		GSRigidBody.cpp \
+		HarrierRigidBody.cpp \
+		HeightTerrainData.cpp \
+		Hydraulics.cpp \
+		JSONRigidBody.cpp \
+		JSONRigidBodyBuilder.cpp \
+		JSONRigidBodyCollection.cpp \
+		LondonGroundTile.cpp \
+		MeshModel.cpp \
+		MipMapTerrainMeshModel.cpp \
+		MipMapTerrainMeshModelCollection.cpp \
+		OpenGLFrameBuffer.cpp \
+		OpenGLRenderBuffer.cpp \
+		OpenGLShaderProgram.cpp \
+		PFDView.cpp \
+		PivotObjects.cpp \
+		RigidBody.cpp \
+		RigidBodyMeshManager.cpp \
+		ScaledHeightTerrainData.cpp \
+		SceneryManager.cpp \
+		SimpleSpringModel.cpp \
+		SpringForceGenerator.cpp \
+		SpringModel.cpp \
+		SRTM30HeightTerrainData.cpp \
+		SRTMFilename.cpp \
 		stdafx.cpp \
-		vector3d.cpp \
-		textureloader.cpp
+		TerrainMeshModel.cpp \
+		TextureManager.cpp \
+		TileNode.cpp \
+		TriangleFanQuadNode.cpp \
+		Weather.cpp \
+		WheelSpringForceGenerator.cpp \
+		WorldSystem.cpp \
+		OpenGLVertexBuffer.cpp \
+		OpenGLPipeline.cpp \
+		OpenGLTexture2D.cpp \
+		WindTunnelForceGenerator.cpp \
+		MeshGroupObject.cpp \
+		OpenGLRenderer.cpp \
+		GPSTerrainData.cpp \
+		MeshObject.cpp \
+		MeshData.cpp \
+		MeshSurfaceObject.cpp \
+		TerrainGroupObject.cpp \
+		PerlinNoise.cpp \
+		SkyDome.cpp \
+		CameraView.cpp \
+		CameraViewProvider.cpp \
+		TurnDirection.cpp \
+		OpenGLFontMeshBuilder.cpp \
+		OpenGLFontRenderer2D.cpp \
+		OpenGLFontTexture.cpp \
+		OpenGLButtonTexture.cpp \
+		OpenGLButtonTextureManager.cpp \
+		OpenGLTextureRenderer2D.cpp \
+		OpenGLSliderControl.cpp \
+		SDLJoystickSystem.cpp \
+		HeathrowRadar.cpp \
+		OpenGLPainter.cpp
 		
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image
 LOCAL_LDLIBS := -lGLESv2 -lGLESv1_CM -llog
